e0497dcee40970391b86c1e53b4526543ff7a2e4
Nimeji <yimeji95@gmail.com>
2021-08-04 20:19:22 +0200

    added feature component

---
+++ src/components/Feature/Feature.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react"
+import Feature from "./Feature";
+
+it('renders the correct titel', () => {
+  render(<Feature img="" title="Test 123"></Feature>);
+
+  expect(screen.getByRole('heading')).toHaveTextContent('Test 123');
+});
+
+it('renders its children', () => {
+  render(<Feature img="">test 123</Feature>);
+
+  expect(screen.getByText('test 123')).toBeInTheDocument();
+});
+
+it('renders the correct image', () => {
+  render(<Feature img="test/123.jpg" />);
+  const img = screen.getByRole('img');
+  expect(img).toHaveAttribute('src', 'test/123.jpg');
+});
---
+++ src/components/Feature/Feature.tsx
@@ -0,0 +1,24 @@
+import { ReactNode } from "react";
+import { Col, Row } from "react-bootstrap";
+
+type FeatureProps = {
+  children?: ReactNode;
+  title?: string;
+  img: string;
+}
+
+export default function Feature({children, title, img}: FeatureProps) {
+  return (
+    <Row>
+      <Col md="7">
+        <h1>
+          {title}
+        </h1>
+        {children}
+      </Col>
+      <Col md="5">
+        <img src={img} alt="" />
+      </Col>
+    </Row>
+  );
+}
