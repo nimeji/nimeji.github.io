0b298546da0b65ac42f536db3bb232558ead5633
Nimeji <yimeji95@gmail.com>
2021-08-05 13:34:02 +0200

    added styling for Feature

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Container } from 'react-bootstrap';
+import Feature from '../Feature/Feature';
+import Divider from '../Divider/Divider';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Divider />
+      <Feature title="Feature 1">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sagittis nibh vel feugiat pharetra. Proin nunc augue, placerat cursus lacus vitae, dictum imperdiet tellus. Sed dui ex, pretium posuere molestie sit amet, auctor ornare felis. Praesent quis nunc eget nunc ultricies porta ut a arcu. Nam eu lorem lacus. Cras feugiat orci laoreet lorem vehicula commodo. In placerat sem quis enim molestie varius. Morbi id luctus sem. Aenean hendrerit eget ligula eget ullamcorper. Maecenas hendrerit tortor consequat commodo auctor. Ut eget tempus mauris, ac tempus odio. Ut pharetra leo erat, sed mattis eros elementum at. Pellentesque maximus rhoncus nisi, in lobortis elit laoreet a. Morbi hendrerit erat vel risus elementum lobortis.
+      </Feature>
+      <Divider />
+      <Feature title="Feature 2" left>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sagittis nibh vel feugiat pharetra. Proin nunc augue, placerat cursus lacus vitae, dictum imperdiet tellus. Sed dui ex, pretium posuere molestie sit amet, auctor ornare felis. Praesent quis nunc eget nunc ultricies porta ut a arcu. Nam eu lorem lacus. Cras feugiat orci laoreet lorem vehicula commodo. In placerat sem quis enim molestie varius. Morbi id luctus sem. Aenean hendrerit eget ligula eget ullamcorper. Maecenas hendrerit tortor consequat commodo auctor. Ut eget tempus mauris, ac tempus odio. Ut pharetra leo erat, sed mattis eros elementum at. Pellentesque maximus rhoncus nisi, in lobortis elit laoreet a. Morbi hendrerit erat vel risus elementum lobortis.
+      </Feature>
+      <Divider />
+    </Container>
   );
 }
 
--- src/components/Feature/Feature.tsx
+++ src/components/Feature/Feature.tsx
@@ -1,24 +1,31 @@
 import { ReactNode } from "react";
 import { Col, Row } from "react-bootstrap";
+import styles from './Feature.module.scss';
 
 type FeatureProps = {
   children?: ReactNode;
   title?: string;
   img?: string;
+  left?: boolean;
 }
 
-export default function Feature({children, title, img}: FeatureProps) {
+export default function Feature({children, title, img="500x500.jpg", left=false}: FeatureProps) {
+  const imageColumn = (
+    <Col md="5" className={styles['img-container']}>
+      <img src={img} alt=""  className={styles['img']}/>
+    </Col>
+  );
+  
   return (
     <Row>
+      {left ? imageColumn : undefined}
       <Col md="7">
         <h1>
           {title}
         </h1>
         {children}
       </Col>
-      <Col md="5">
-        <img src={img} alt="" />
-      </Col>
+      {left ? undefined : imageColumn}
     </Row>
   );
 }
