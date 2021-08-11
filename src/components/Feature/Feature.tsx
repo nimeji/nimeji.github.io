55f4bd545f8305269c3ff9ace52f07f2a824a8c5
Nimeji <yimeji95@gmail.com>
2021-08-11 20:48:29 +0200

    added markdown support to feature component, include placeholder and sav summary

--- src/components/Feature/Feature.tsx
+++ src/components/Feature/Feature.tsx
@@ -1,15 +1,27 @@
-import { ReactNode } from "react";
+import { useEffect, useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import styles from './Feature.module.scss';
+import ReactMarkdown from 'react-markdown';
 
 type FeatureProps = {
-  children?: ReactNode;
-  title?: string;
+  summaryPath?: string;
+  detailsPath?: string;
   img?: string;
   left?: boolean;
 }
 
-export default function Feature({children, title, img="500x500.jpg", left=false}: FeatureProps) {
+export default function Feature({summaryPath, detailsPath, img="500x500.jpg", left=false}: FeatureProps) {
+  const [summary, setSummary] = useState('');
+  const [details, setDetails] = useState('');
+
+  useEffect(() => {
+    if(summaryPath) fetch(summaryPath).then(r => r.text()).then(text => setSummary(text));
+  }, [summaryPath]);
+
+  useEffect(() => {
+    if(detailsPath) fetch(detailsPath).then(r => r.text()).then(text => setDetails(text));
+  }, [detailsPath]);
+
   const imageColumn = (
     <Col md="5" className={styles['img-container']}>
       <img src={img} alt=""  className={styles['img']}/>
@@ -20,10 +32,7 @@ export default function Feature({children, title, img="500x500.jpg", left=false}
     <Row>
       {left ? imageColumn : undefined}
       <Col md="7">
-        <h1>
-          {title}
-        </h1>
-        {children}
+        <ReactMarkdown>{summary}</ReactMarkdown>
       </Col>
       {left ? undefined : imageColumn}
     </Row>
---
+++ src/websites.ts
@@ -0,0 +1,22 @@
+type Website = {
+  summary?: string;
+  details?: string;
+  img?: string;
+}
+
+const websites: Website[] = [
+  {
+    summary: 'content/sav-summary.md',
+    img: 'content/sav.png',
+  },
+  {
+    summary: 'content/test.md',
+    img: '500x500.jpg',
+  },
+  {
+    summary: 'content/test.md',
+    img: '500x500.jpg',
+  }
+]
+
+export default websites;
