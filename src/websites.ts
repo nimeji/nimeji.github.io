c052fe460dca521b7cf2e6456378d269f5574314
Nimeji <yimeji95@gmail.com>
2021-08-11 21:46:04 +0200

    added some more websites for testing

--- src/websites.ts
+++ src/websites.ts
@@ -2,12 +2,22 @@ type Website = {
   summary?: string;
   details?: string;
   img?: string;
+  link?: string;
 }
 
 const websites: Website[] = [
   {
     summary: 'content/sav-summary.md',
     img: 'content/sav.png',
+    link: 'https://nimeji.github.io/sorting-algorithm-visualizer/',
+  },
+  {
+    summary: 'content/test.md',
+    img: '500x500.jpg',
+  },
+  {
+    summary: 'content/test.md',
+    img: '500x500.jpg',
   },
   {
     summary: 'content/test.md',
