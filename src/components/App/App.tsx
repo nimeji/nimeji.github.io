fd4bb812db9f68713d2746532f6a238a8cdb08ff
Nimeji <yimeji95@gmail.com>
2021-08-23 13:55:02 +0200

    updated layout

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,25 +1,43 @@
 import React from 'react';
-import './App.css';
-import { Col, Row, Container } from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
 import Feature from '../Feature/Feature';
-import Divider from '../Divider/Divider';
 import websites from '../../websites';
+import { Discord, EnvelopeFill, Linkedin } from 'react-bootstrap-icons';
+import styles from './App.module.scss';
 
 function App() {
   return (
-    <Container>
-      <Divider />
+    <div className="px-5 py-3 bg-dark">
       <Row>
-        {
-          websites.map((website, i) => (
-            <Col key={i} lg={4} className="py-2">
-              <Feature content={website.content} images={website.images} demo={website.demo} github={website.github} />
-            </Col>
-          ))
-        }
+        <Col xl={4} xxl={3} className="text-light pb-5">
+          <h1>Max Schrodt</h1>
+          <p>Im an aspiring frontend developer from germany. I usually work with Typescript, React and SASS. I also have some experience using various UI/UX libraries like Bootstrap and MaterialUI. </p>
+          <p>On this website you can find information on some of my previous projects.</p>
+          <h4>Contact</h4>
+          <div className={styles.FlexColumn}>
+            <span><Linkedin /> Linkedin</span>
+            <span><EnvelopeFill /> E-Mail</span>
+            <span><Discord /> Discord</span>
+          </div>
+          <div className={styles.FlexColumn}>
+            <a href="https://www.linkedin.com/in/max-schrodt-768036218/" className="text-light">Max Schrodt</a>
+            <a href="mailto: maxschrodt@gmx.net" className="text-light">maxschrodt@gmx.net</a>
+            <span>Nimeji#0719</span>
+          </div>
+        </Col>
+        <Col>
+          <Row>
+            {
+              websites.map((website, i) => (
+                <Col key={i} sm={12} md={6} xxl={4} className="py-2">
+                  <Feature content={website.content} images={website.images} demo={website.demo} github={website.github} />
+                </Col>
+              ))
+            }
+          </Row>
+        </Col>
       </Row>
-      <Divider />
-    </Container>
+    </div>
   );
 }
 
